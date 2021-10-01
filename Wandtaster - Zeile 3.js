0beead72fbd8ab0ff26acbd0252a0396d61b8a27
pogh <>
2021-10-01 08:18:57 +0200

    Icon Spacing

--- Wandtaster - Zeile 3.js
+++ Wandtaster - Zeile 3.js
@@ -9,17 +9,17 @@ string beep = "";
 if(dom.GetObject("FensterOffen").Value() == true)
 {
     icon = "24";
-    text = text # "  ";
 }
-else
-{
-    icon = "";
-};
 
 if(dom.GetObject("NiedrigtempWarnung").Value() == true)
 {
     icon = "7";
     beep = ",{R=2,IN=5,ANS=7}";
+}
+
+if(icon != "")
+{
+    text = text # "  ";
 }
 
 dom.GetObject("HmIP-RF.002A5D8989D5D9:3.COMBINED_PARAMETER").State("{DDBC=WHITE,DDTC=BLACK,DDI=" # icon # ",DDA=CENTER,DDS=" # text # ",DDID=3,DDC=true}" # beep);
