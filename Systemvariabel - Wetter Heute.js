b740d9ef384d6725ea71af96294c1a7484d5138b
pogh <>
2024-05-20 09:37:55 +0200

    Moved from OpenWetherMap to OpenMeteo

--- Systemvariabel - Wetter Heute.js
+++ Systemvariabel - Wetter Heute.js
@@ -1,9 +1,26 @@
 !----------------------------------------------------------------------------- !
-    !WebService
+! Icons
+
+! 10 = Sonne
+! 11 = Mond
+! 12 = Wind
+! 13 = Wolke
+! 14 = Wolke/Blitz
+! 15 = Wolke/leichter Regen
+! 16 = Wolke/Mond
+! 17 = Wolke/Regen
+! 18 = Wolke/Schnee
+! 19 = Wolke/Sonne
+! 20 = Wolke/Sonne/Regen
+! 21 = Wolke/Schneeflocke
+! 22 = Wolke/Regentropfen
+
+!----------------------------------------------------------------------------- !
+!WebService
 
 string stdout;
 string stderr;
-string url = "https://api.openweathermap.org/data/2.5/onecall?lat=52.5220&lon=13.4133&mode=json&units=metric&exclude=current,daily,minutely&appid=" # dom.GetObject("OpenWeatherMapApiKey").Value();
+string url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&forecast_hours=12&hourly=temperature_2m,dew_point_2m,weather_code&timezone=Europe%2FBerlin";
 
 system.Exec("wget -q --no-check-certificate -O - '" # url # "' ", & stdout, & stderr);
 
@@ -12,153 +29,350 @@ system.Exec("wget -q --no-check-certificate -O - '" # url # "' ", & stdout, & st
 real realValue;
 integer integerValue;
 
-real min = 99.9;
-real max = 00.0;
-real dewPoint = 0.0;
-
-integer icon = 0;
-string iconString;
-integer id = 0;
-integer clearSkyIconCount = 0;
-integer cloudIconCount = 0;
-
+string values;
 string tagName;
 integer tagNameLength;
 string tagValue;
 
-!----------------------------------------------------------------------------- !
+integer hours_ahead_count = 12;   ! Same as in URL
+integer hour = system.Date("%H").ToInteger();
+boolean is_night = true;
+
+real temp_min = 99.9;
+real temp_max = 00.0;
+
+real first_temp = 99.9;
+integer temp_rising_count = 0;
+integer temp_falling_count = 0;
+boolean wetter_temp_rising = true;
+
+real temp_dew_point = 00.0;
+
+integer weather_code = -1;
+string icon;
+
+!------------------------------------------------------------------------------!
 
 if (stdout.Length() > 0) {
-integer i = 0;
-    while (i < 12) {
+
+    !--------------------------------------------------------------------------!
+    ! Min, Max Temps
+
+    integer i = 0;
+    tagName = "temperature_2m";
+    tagNameLength = tagName.Length();
+
+    values = stdout;
+    values = values.Substr(values.Find("\"" + tagName + "\":") + tagNameLength + 3);
+    values = values.Substr(values.Find("\"" + tagName + "\":") + tagNameLength + 4);
+
+    while (i < hours_ahead_count) {
         i = i + 1;
 
-        tagName = "temp";
-        tagNameLength = tagName.Length();
+        tagValue = values.Substr(0, values.Find(","));
+        values = values.Substr(tagValue.Length() + 1);
 
-        stdout = stdout.Substr(stdout.Find("\"" + tagName + "\":"));
-        tagValue = stdout.Substr(0, stdout.Find(","));
-        tagValue = tagValue.Substr(tagNameLength + 3);
-
-        !WriteLine(i # " temp: " # tagValue);
+        !WriteLine(i # " temperature_2m: " # tagValue);
 
         realValue = tagValue.ToFloat();
 
-        if (realValue < min) {
-            min = realValue;
+        if (realValue < temp_min) {
+            temp_min = realValue;
         }
 
-        if (realValue > max) {
-            max = realValue;
+        if (realValue > temp_max) {
+            temp_max = realValue;
         }
 
-        ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - !
+        if(first_temp == 99.9)
+        {
+            first_temp = realValue;
+        }
+        else
+        {
+            if(realValue > first_temp)
+            {
+                temp_rising_count = temp_rising_count + 1;
+            }
+            else
+            {
+                temp_falling_count = temp_falling_count + 1;
+            }
+        }
+    }
+    
+    !WriteLine(temp_rising_count);
+    !WriteLine(temp_falling_count);
 
-        tagName = "dew_point";
-        tagNameLength = tagName.Length();
+    if(temp_rising_count > temp_falling_count)
+    {
+        wetter_temp_rising = true;
+    }
+    else
+    {
+        wetter_temp_rising = false;
+    }
 
-        stdout = stdout.Substr(stdout.Find("\"" + tagName + "\":"));
-        tagValue = stdout.Substr(0, stdout.Find(","));
-        tagValue = tagValue.Substr(tagNameLength + 3);
+    !WriteLine(temp_min);
+    !WriteLine(temp_max);
+    !WriteLine(wetter_temp_rising);
 
-        !WriteLine(i # " dew_point: " # tagValue);
+    !--------------------------------------------------------------------------!
+    ! Dew Point Temperature
+
+    integer i = 0;
+    tagName = "dew_point_2m";
+    tagNameLength = tagName.Length();
+
+    values = stdout;
+    values = values.Substr(values.Find("\"" + tagName + "\":") + tagNameLength + 3);
+    values = values.Substr(values.Find("\"" + tagName + "\":") + tagNameLength + 4);
+
+    while (i < hours_ahead_count) {
+        i = i + 1;
+
+        tagValue = values.Substr(0, values.Find(","));
+        values = values.Substr(tagValue.Length() + 1);
+
+        !WriteLine(i # " dew_point_2m: " # tagValue);
 
         realValue = tagValue.ToFloat();
 
-        if (realValue > dewPoint) {
-            dewPoint = realValue;
+        if (realValue > temp_dew_point) {
+            temp_dew_point = realValue;
         }
+    }
 
-        ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - !
+    !WriteLine(temp_dew_point);
 
-        tagName = "id";
-        tagNameLength = tagName.Length();
+    !--------------------------------------------------------------------------!
+    ! Weather Icon
 
-        stdout = stdout.Substr(stdout.Find("\"" + tagName + "\":"));
-        tagValue = stdout.Substr(0, stdout.Find(","));
-        tagValue = tagValue.Substr(tagNameLength + 3);
+    integer i = 0;
+    tagName = "weather_code";
+    tagNameLength = tagName.Length();
 
-        !WriteLine(i # " id: " # tagValue);
+    values = stdout;
+    values = values.Substr(values.Find("\"" + tagName + "\":") + tagNameLength + 3);
+    values = values.Substr(values.Find("\"" + tagName + "\":") + tagNameLength + 4);
 
-        id = tagValue.Substr(0, 3).ToInteger();
+    while (i < hours_ahead_count) {
+        i = i + 1;
 
-        ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - !
+        tagValue = values.Substr(0, values.Find(","));
+        values = values.Substr(tagValue.Length() + 1);
 
-        tagName = "icon";
-        tagNameLength = tagName.Length();
+        !WriteLine(i # " weather_code: " # tagValue);
 
-        stdout = stdout.Substr(stdout.Find("\"" + tagName + "\":"));
-        tagValue = stdout.Substr(0, stdout.Find(",") - 2);
-        tagValue = tagValue.Substr(tagNameLength + 3);
-        tagValue = tagValue.Substr(1, tagValue.Length() - 2);
+        integerValue = tagValue.ToInteger();
 
-        !WriteLine(i # " icon: " # tagValue);
-
-        integerValue = tagValue.Substr(0, 2).ToInteger();
-
-        !Change 'broken clouds: 51%-84%' icon to 'scattered clouds' icon
-        if (id == 803) {
-            integerValue = 3;
-            iconString = "03x";
+        if (integerValue > weather_code) {
+            weather_code = integerValue;
         }
+    }
+    
+    !WriteLine(weather_code);
+    
+    if(hour < 4 || hour > 5)
+    {
+        is_night = true;
+    }
+    else
+    {
+        is_night = false;
+    }
+    
+    icon = "05";
 
-        if (integerValue < 4) {
-            clearSkyIconCount = clearSkyIconCount + 1;
+    ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - !
+    ! 0  Clear sky
+    if(weather_code == 0) 
+    {
+        if(!is_night)
+        {
+            icon = "10"; ! Sonne
         }
+        else
+        {
+            icon = "11"; ! Mond
+        }
+    }
 
-        if (integerValue == 4) {
-            cloudIconCount = cloudIconCount + 1;
+    ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - !
+    ! 1, 2, 3  Mainly clear, partly cloudy, and overcast
+    if(weather_code == 1) 
+    {
+        if(!is_night)
+        {
+            icon = "10"; ! Sonne
         }
+        else
+        {
+            icon = "11"; ! Mond
+        }
+    }
+    if(weather_code == 2)
+    {
+        if(!is_night)
+        {
+            icon = "19"; ! Wolke/Sonne
+        }
+        else
+        {
+            icon = "16"; ! Wolke/Mond
+        }
+    }
+    if(weather_code == 3) 
+    {
+        if(!is_night)
+        {
+            icon = "19"; ! Wolke/Sonne
+        }
+        else
+        {
+            icon = "16"; ! Wolke/Mond
+        }
+    }
 
-        if (integerValue > icon) {
-            icon = integerValue;
-            iconString = tagValue;
-        }
+    ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - !
+    ! 45, 48  Fog and depositing rime fog
+    if(weather_code == 45) 
+    {
+        icon = "13"; ! Wolke
+    }
+    if(weather_code == 48) 
+    {
+        icon = "13"; ! Wolke
+    }
+
+    ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - !
+    ! 51, 53, 55  Drizzle: Light, moderate, and dense intensity
+    if(weather_code == 51) 
+    {
+        icon = "15"; ! Wolke/leichter Regen
+    }
+    if(weather_code == 53) 
+    {
+        icon = "17"; ! Wolke/Regen
+    }
+    if(weather_code == 55) 
+    {
+        icon = "17"; ! Wolke/Regen
+    }
+
+    ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - !
+    ! 56, 57  Freezing Drizzle: Light and dense intensity
+    if(weather_code == 56) 
+    {
+        icon = "15"; ! Wolke/leichter Regen
+    }
+    if(weather_code == 57) 
+    {
+        icon = "17"; ! Wolke/Regen
+    }
+
+    ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - !
+    !61, 63, 65  Rain: Slight, moderate and heavy intensity
+    if(weather_code == 61)
+    {
+        icon = "15"; ! Wolke/leichter Regen
+    }
+    if(weather_code == 63)
+    {
+        icon = "17"; ! Wolke/Regen
+    }
+    if(weather_code == 65) 
+    {
+        icon = "17"; ! Wolke/Regen
+    }
+
+    !66, 67  Freezing Rain: Light and heavy intensity
+    if(weather_code == 66) 
+    {
+        icon = "15"; ! Wolke/leichter Regen
+    }
+    if(weather_code == 67)
+    {
+        icon = "17"; ! Wolke/Regen
+    }
+
+    ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - !
+    !71, 73, 75  Snow fall: Slight, moderate, and heavy intensity
+    if(weather_code == 71) 
+    {
+        icon = "18"; ! Wolke/Schnee
+    }
+    if(weather_code == 73) 
+    {
+        icon = "18"; ! Wolke/Schnee
+    }
+    if(weather_code == 75) 
+    {
+        icon = "18"; ! Wolke/Schnee
+    }
+
+    !77  Snow grains
+    if(weather_code == 77) 
+    {
+        icon = "18"; ! Wolke/Schnee
+    }
+
+    ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - !
+    !80, 81, 82  Rain showers: Slight, moderate, and violent
+    if(weather_code == 80) 
+    {
+        icon = "20"; ! Wolke/Sonne/Regen
+    }
+    if(weather_code == 81) 
+    {
+        icon = "20"; ! Wolke/Sonne/Regen
+    }
+    if(weather_code == 82) 
+    {
+        icon = "20"; ! Wolke/Sonne/Regen
+    }
+
+    !85, 86  Snow showers slight and heavy
+    if(weather_code == 85) 
+    {
+        icon = "21"; ! Wolke/Schneeflocke
+    }
+    if(weather_code == 86) 
+    {
+        icon = "21"; ! Wolke/Schneeflocke
+    }
+
+    ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - !
+    !95 *  Thunderstorm: Slight or moderate
+    if(weather_code == 95) 
+    {
+        icon = "14"; ! Wolke/Blitz;
+    }
+
+    !96, 99 *  Thunderstorm with slight and heavy hail
+    if(weather_code == 96) 
+    {
+        icon = "14"; ! Wolke/Blitz;
+    }
+    if(weather_code == 99)
+    {
+        icon = "14"; ! Wolke/Blitz;
     }
 
     !----------------------------------------------------------------------------- !
 
-    if ((iconString.Substr(0, 2).ToInteger() <= 4)
-        && (clearSkyIconCount > 0)
-        && (cloudIconCount > 0)) {
-        iconString = "03x";
-    }
-
-    !----------------------------------------------------------------------------- !
-
-    tagName = "alerts";
-
-    if (stdout.Find("\"" + tagName + "\":") > -1) {
-        stdout = stdout.Substr(stdout.Find("\"" + tagName + "\":"));
-
-        tagName = "event";
-        tagNameLength = tagName.Length();
-
-        stdout = stdout.Substr(stdout.Find("\"" + tagName + "\":"));
-        tagValue = stdout.Substr(1, stdout.Find(",\"") - 2);
-        tagValue = tagValue.Substr(tagNameLength + 3);
-
-        tagValue = tagValue.ToLatin();
-        
-        dom.GetObject("WetterAlert").State(tagValue);
-        WriteLine("Alert: " # tagValue);
-    }
-    else {
-        dom.GetObject("WetterAlert").State("");
-    }
-
-    !----------------------------------------------------------------------------- !
-
-    dom.GetObject("WetterTempMin").State(min);
-    dom.GetObject("WetterTempMax").State(max);
-    dom.GetObject("WetterTempDewPoint").State(dewPoint);
-    dom.GetObject("WetterIcon").State(iconString);
+    dom.GetObject("WetterTempMin").State(temp_min);
+    dom.GetObject("WetterTempMax").State(temp_max);
+    dom.GetObject("WetterTempRising").State(wetter_temp_rising);
+    dom.GetObject("WetterTempDewPoint").State(temp_dew_point);
+    dom.GetObject("WetterIcon").State(icon);
 
     !----------------------------------------------------------------------------- !
     !Debug
 
     WriteLine("Min: " # dom.GetObject("WetterTempMin").Value());
     WriteLine("Max: " # dom.GetObject("WetterTempMax").Value());
+    WriteLine("WetterTempRising: " # dom.GetObject("WetterTempRising").Value());
     WriteLine("Dew Point: " # dom.GetObject("WetterTempDewPoint").Value());
     WriteLine("Icon: " # dom.GetObject("WetterIcon").Value());
 }
--- Wandtaster - Zeile 2.js
+++ Wandtaster - Zeile 2.js
@@ -1,172 +1,25 @@
-!-----------------------------------------------------------------------------!
-! Day icon 	Night icon 	Description
-! 01d.png 	01n.png 	clear sky
-! 02d.png 	02n.png 	few clouds
-! 03d.png 	03n.png 	scattered clouds
-! 04d.png 	04n.png 	broken clouds
-! 09d.png 	09n.png 	shower rain
-! 10d.png 	10n.png 	rain
-! 11d.png 	11n.png 	thunderstorm
-! 13d.png 	13n.png 	snow
-! 50d.png 	50n.png 	mist 
-
-!-----------------------------------------------------------------------------!
-! 10 = Sonne
-! 11 = Mond
-! 12 = Wind
-! 13 = Wolke
-! 14 = Wolke/Blitz
-! 15 = Wolke/leichter Regen
-! 16 = Wolke/Mond
-! 17 = Wolke/Regen
-! 18 = Wolke/Schnee
-! 19 = Wolke/Sonne
-! 20 = Wolke/Sonne/Regen
-! 21 = Wolke/Schneeflocke
-! 22 = Wolke/Regentropfen
-
-!-----------------------------------------------------------------------------!
-
 real temp;
 string icon = "0";
 string text = "";
 string beep = "";
 
-string iconVariable = dom.GetObject("WetterIcon").Value().Substr(0, 2);
+! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - !
 
-if((system.Date("%H").ToInteger() >= 3)
-&& (system.Date("%H").ToInteger() < 17))
+if(dom.GetObject("WetterTempRising").Value() == true)
 {
-
     temp = dom.GetObject("WetterTempMax").Value();
-    temp = temp.Round(0);
-    text = temp.ToString();
-    text = text.Substr(0, text.Find(".") + 0) + dom.GetObject("Gradzeichen").Value() + "C";  ! Find() + 0 ==> Homematic Bug
-
-    if(dom.GetObject("WetterTempDewPoint").Value() > 16)
-    {
-        text = "Frizz! " + text;
-    }
-
-    icon = "10"; ! Sonne
-
-    !clear sky
-    if(iconVariable == "01")
-    { 	
-        icon = "10"; ! Sonne
-    }
-    
-    !few clouds: 11 - 25 %
-    if(iconVariable == "02")  
-    {
-        icon = "10"; ! Sonne
-    }
-    
-    !scattered clouds: 25 - 50 %
-    if(iconVariable == "03")  
-    { 	
-        icon = "19";  ! Wolke/Sonne
-    }
-    
-    !broken clouds: 51 - 100 %
-    if(iconVariable == "04")  
-    { 	
-        icon = "13";  ! Wolke
-    }
-    
-    !shower rain
-    if(iconVariable == "09")  
-    { 	
-        icon = "17";  ! Wolke/Regen
-    }
-    
-    !rain
-    if(iconVariable == "10")  
-    { 	
-        icon = "17";  ! Wolke/Regen
-    }
-    
-    !thunderstorm
-    if(iconVariable == "11")  
-    { 	
-        icon = "14";  ! Wolke/Blitz
-    }
-    
-    !snow
-    if(iconVariable == "13")  
-    { 	
-        icon = "18";  ! Wolke/Schnee
-    }
-    
-    !mist
-    if(iconVariable == "50")   
-    { 	
-        icon = "13";  ! Wolke
-    }
 }
 else
 {
     temp = dom.GetObject("WetterTempMin").Value();
-    temp = temp.Round(0);
-    text = temp.ToString();
-    text = text.Substr(0, text.Find(".") + 0) + dom.GetObject("Gradzeichen").Value() + "C";  ! Find() + 0 ==> Homematic Bug
-
-    icon = "11"; ! Mond
-
-    !clear sky
-    if(iconVariable == "01")  
-    { 	
-        icon = "11"; ! Mond
-    }
-    
-    !few clouds: 11 - 25 %
-    if(iconVariable == "02")  
-    {
-        icon = "11"; ! Mond
-    }
-    
-    !scattered clouds: 25 - 50 %
-    if(iconVariable == "03")  
-    { 	
-        icon = "16"; ! Wolke/Mond
-    }
-    
-    !broken clouds: 51 - 100 %
-    if(iconVariable == "04")  
-    { 	
-        icon = "13";  ! Wolke
-    }
-    
-    !shower rain
-    if(iconVariable == "09")  
-    { 	
-        icon = "17";  ! Wolke/Regen
-    }
-    
-    !rain
-    if(iconVariable == "10")  
-    { 	
-        icon = "17";  ! Wolke/Regen
-    }
-    
-    !thunderstorm
-    if(iconVariable == "11")  
-    { 	
-        icon = "14";  ! Wolke/Blitz
-    }
-    
-    !snow
-    if(iconVariable == "13")  
-    { 	
-        icon = "18";  ! Wolke/Schnee
-    }
-    
-    !mist
-    if(iconVariable == "50")   
-    { 	
-        icon = "13";  ! Wolke
-    }
 }
+temp = temp.Round(0);
+
+text = temp.ToString();
+
+! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - !
+
+text = text.Substr(0, text.Find(".") + 0) + dom.GetObject("Gradzeichen").Value() + "C";  ! Find() + 0 ==> Homematic Bug
 
 text = text.Replace("ä", "{");
 text = text.Replace("ö", "|");
@@ -177,4 +30,10 @@ text = text.Replace("Ü", "$");
 text = text.Replace("ß", "_");
 text = text.Replace("°", dom.GetObject("Gradzeichen").Value());
 
-dom.GetObject("HmIP-RF.002A5D8989D5D9:3.COMBINED_PARAMETER").State("{DDBC=WHITE,DDTC=BLACK,DDI=" # icon # ",DDA=CENTER,DDS=" # text # ",DDID=2,DDC=true}" # beep);
+! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - !
+
+icon = dom.GetObject("WetterIcon").Value();
+
+! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - !
+
+dom.GetObject("HmIP-RF.002A5D8989D5D9:3.COMBINED_PARAMETER").State("{DDBC=WHITE,DDTC=BLACK,DDI=" # icon # ",DDA=CENTER,DDS=" # text # " ,DDID=2,DDC=true}" # beep);
