aafb90ae3d2cef3a5d3203a9bacfae58eed6239f
pogh <>
2024-05-31 07:06:31 +0200

    Added device names to low battery warning

--- Systemvariabel - AnzahlNiedrigeBatterien.js
+++ Systemvariabel - AnzahlNiedrigeBatterien.js
@@ -1,32 +1,44 @@
 integer count = 0;
 integer lowBatCount = 0;
+string lowBatNames = "";
 
 string id;
 foreach(id, dom.GetObject(ID_DEVICES).EnumUsedIDs())
 {
-count = count + 1;
+    count = count + 1;
 
-var device = dom.GetObject(id);
-var ch = device.Channels().GetAt(0);
+    var device = dom.GetObject(id);
+    var ch = device.Channels().GetAt(0);
 
-var dp = ch.DPByHssDP("LOWBAT");
-if (!dp) {
-    var dp = ch.DPByHssDP("LOW_BAT");
- }
+    var dp = ch.DPByHssDP("LOWBAT");
+    if (!dp) {
+        var dp = ch.DPByHssDP("LOW_BAT");
+    }
 
- if ( dp.State() && dp.Value() ) {
-     lowBatCount = lowBatCount + 1;
-     WriteLine(device.Name());
- }
+    if ( dp.State() && dp.Value() ) {
+        lowBatCount = lowBatCount + 1;
 
-!WriteLine(device.Name());
+        if(lowBatNames.Length() > 0)
+        {
+            lowBatNames = lowBatNames # ", ";
+        }
+        lowBatNames = lowBatNames # device.Name();
+
+        WriteLine(device.Name());
+    }
+
+    !WriteLine(device.Name());
 }
 
 if(dom.GetObject("AnzahlNiedrigeBatterien").Value() != lowBatCount)
 {
-dom.GetObject("AnzahlNiedrigeBatterien").State(lowBatCount);
-WriteLine("New AnzahlNiedrigeBatterien: " # lowBatCount);
+    dom.GetObject("AnzahlNiedrigeBatterien").State(lowBatCount);
+    WriteLine("New AnzahlNiedrigeBatterien: " # lowBatCount);
+
+    dom.GetObject("NamenNiedrigeBatterien").State(lowBatNames);
+    WriteLine("New NamenNiedrigeBatterien: " # lowBatNames);
 }
 
 WriteLine("Count: " # count);
 WriteLine("Low Battery Count: " # lowBatCount);
+WriteLine("Low Battery Devices: " # lowBatNames);
